d284a61558d66a51f4a2de7aceedb3dfd66df79b
Wladyslaw Figura <wfigura@op.pl>
2026-07-18 11:43:39 +0100

    init

---
+++ ApiRestaurant/src/main/java/figura/ApiRestaurant/ApiRestaurantApplication.java
@@ -0,0 +1,13 @@
+package figura.ApiRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestaurantApplication.class, args);
+	}
+
+}
